73ecc41ba15c6e2382cbe68da9ef6b3436325e0d
RainbowHotkey <hotkeysplayer@gmail.com>
2021-11-02 02:12:04 +0300

    pre-finish

--- BattleGame.cpp
+++ BattleGame.cpp
@@ -431,7 +431,263 @@ int main()
                         }
                     }
                 }
+                int hit;
+                if (ourHero.getLevel() == 4)
+                    switch (yon)
+                    {
+                        case 'y':
+                        {
+                            cout << "The Queen of Blades itself stands in front of you!";
+                            character Kerrigan("Kerrigan", 800, 50, 30, 1, 0, 0);
+
+                            while (Kerrigan.getHp() > 0)
+                            {
+                                cout << "What will you do?\n\n"
+                                    << "[1] Attack"
+                                    << "[2] Call for evacuation";
+                                cin >> combat;
+                                system("cls");
+                                switch (combat)
+                                {
+                                case 1:
+                                {
+                                    if ((ourHero.getStr() + (rand() % 11)) > (Kerrigan.getStr() + (rand() % 11)))
+                                    {
+                                        hit = ourHero.getStr() * (ourWeapon.getDamageMultiplier());
+                                        cout << "You hit Kerrigan for " << hit << "\n\n";
+                                        current_hp = Kerrigan.getHp() - hit;
+                                        Kerrigan.setHp(current_hp);
+                                        if (1 > Kerrigan.getHp())
+                                        {
+                                            cout << "You have seriously injured The Queen of Blades\n\n";
+                                            Sleep(3000);
+                                            system("cls");
+                                            cout << "The Emperror Mengsk proud of you!\n"
+                                                << "You have succesfully complete this task and now you, "
+                                                << name << " are the proud of Dominion";
+                                            cout << "Please press enter...";
+                                            cin.ignore();
+                                            cin.get();
+                                            ourHero.setHp(0);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        cout << "You missed\n\n";
+                                    }
+                                    if (Kerrigan.getHp() > 0)
+                                    {
+                                        if ((Kerrigan.getStr() + (rand() % 11)) > (ourHero.getStr() + (rand() % 11)))
+                                        {
+                                            hit = Kerrigan.getStr() * 3.3;
+                                            hit = hit * ourHero.getArmor();
+                                            cout << "Kerrigan deals you " << hit << " damage!\n\n";
+                                            current_hp = ourHero.getHp() - hit;
+                                            ourHero.setHp(current_hp);
+                                        }
+                                        else
+                                        {
+                                            cout << "Kerrigan missed";
+                                        }
+                                    }
+                                    cin.ignore();
+                                    cin.get();
+                                    system("cls");
+                                    break;
+                                }
+                                case 2:
+                                {
+                                    cout << "That was wrong decision...\n";
+                                    Sleep(1500);
+                                    cout << "Kerrigan desintagrates you!";
+                                    Sleep(3000);
+                                    system("cls");
+                                    ourHero.setHp(0);
+                                    Kerrigan.setHp(0);
+                                    break;
+                                }
+                                default:
+                                {
+                                    cout << "Invalid selection\n\n";
+                                    press();
+                                }
+                                }
+                                if (ourHero.getHp() < 1)
+                                {
+                                    cout << "You died";
+                                    Sleep(1700);
+                                    system("cls");
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        default:
+                        {
+                            system("cls");
+                            break;
+                        }
+                    }
+                break;
+            }
+            case 3:
+            {
+                system("cls");
+                cout << "Welcome to the Med-station\n";
+                cout << "You have" << ourHero.getCredits() << " credits!\n\n";
+                cout << "What do you want?\n\n"
+                    << "[1] Light heal - 25\n\n"
+                    << "[2] Medium heal - 45\n\n"
+                    << "[3] Big heal - 100\n\n"
+                    << "[4] Stimpack - 65\n\n"
+                    << "[5] Exit";
+                int choice;
+                cin >> choice;
+                system("cls");
+                int newhp;
+                switch (choice)
+                {
+                    case 1:
+                    {
+                        if (ourHero.getCredits() > 24)
+                        {
+                            newhp = ourHero.getHp() + 20;
+                            ourHero.setHp(newhp);
+                            newCredits = ourHero.getCredits() - 25;
+                            ourHero.setCredits(newCredits);
+                            cout << "A little better";
+                            enter();
+                            break;
+                        }
+                        else
+                        {
+                            cout << "Not enough credits";
+                            enter();
+                            break;
+                        }
+                    }
+                    case 2:
+                    {
+                        if (ourHero.getCredits() > 44)
+                        {
+                            newhp = ourHero.getHp() + 50;
+                            ourHero.setHp(newhp);
+                            newCredits = ourHero.getCredits() - 45;
+                            ourHero.setCredits(newCredits);
+                            cout << "You feel better";
+                            enter();
+                            break;
+                        }
+                        else
+                        {
+                            cout << "Not enough credits";
+                            enter();
+                            break;
+                        }
+                    }
+                    case 3:
+                    {
+                        if (ourHero.getCredits() > 99)
+                        {
+                            newhp = ourHero.getHp() + 110;
+                            ourHero.setHp(newhp);
+                            newCredits = ourHero.getCredits() - 100;
+                            ourHero.setCredits(newCredits);
+                            cout << "Power Overwhelthing!";
+                            enter();
+                            break;
+                        }
+                        else
+                        {
+                            cout << "Not enough credits";
+                            enter();
+                            break;
+                        }
+                    }
+                    case 4:
+                    {
+                        if (ourHero.getCredits() > 64)
+                        {
+                            newStr = ourHero.getStr() + 2;
+                            ourHero.setStr(newStr);
+                            newCredits = ourHero.getCredits() - 65;
+                            ourHero.setCredits(newCredits);
+                            newDoge = ourHero.getDoge() + 2;
+                            ourHero.setDoge(newDoge);
+                            cout << "Задавим врагов интеллектом!!!";
+                            enter();
+                        }
+                        else
+                        {
+                            cout << "Not enough credits";
+                            enter();
+                            break;
+                        }
+                    }
+                    case 5:
+                    {
+                        break;
+                    }
+                    default:
+                    {
+                        cout << "Invalid selection\n\n";
+                        press();
+                    }
+                }
+                break;
+            }
+            case 4:             //statistics
+            {
+                cout << "Your stats: \n"
+                    << "Class: " << ourHero.getClass() << endl
+                    << "Name: " << name << endl
+                    << "Hp: " << ourHero.getHp() << endl
+                    << "Strength: " << ourHero.getStr() << endl
+                    << "Doge: " << ourHero.getDoge() << endl << endl
+                    << "Weapon stats: \n"
+                    << ourWeapon.getWeaponName() << endl
+                    << "Damage: " << ourWeapon.getDamageMultiplier();
+                press();
+            }
+            case 5: //Quit game
+            {
+                cout << "Are you sure?\n [y]es or [n]o";
+                char yes;
+                cin >> yes;
+                if (yes == 'y' || yes == 'Y')
+                    ourHero.setHp(0);
+                system("cls");
+                break;
+            }
+            default:
+            {
+                cout << "Invalid selection\n\n";
+                enter();
             }
         }
     }
+    if (ourHero.getHp() < 1)
+    {
+        gameOver();
+        cout << "You finished with " << experience << " exp";
+        press();
+    }
+    cout << "Play again? \n [y]es or [n]o";
+    cin >> yon;
+    switch (yon)
+    {
+        case 'y':
+        {
+            main();
+            break;
+        }
+        case 'Y':
+        {
+            main();
+            break;
+        }
+    default:
+        break;
+    }
+    return 0;
 }
